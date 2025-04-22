848bcef6de1354ad5c1ac37062dff1a4226117e3
Hugo Anderzon <huan2300@student.miun.se>
2025-04-22 13:08:27 +0200

    Nuget packages

--- SecurityEducation/obj/Debug/net8.0/SecurityEducation.AssemblyInfo.cs
+++ SecurityEducation/obj/Debug/net8.0/SecurityEducation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurityEducation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903f3482ac8d89fa5ab2d21b624daf8b770fd76c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c06433d52a3b7ed55aaee3ca3ee824b58039be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurityEducation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurityEducation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
