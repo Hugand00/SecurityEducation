0314f054bd8adc646c29d3cfd7da0a0202e70735
Hugo Anderzon <huan2300@student.miun.se>
2025-05-22 12:00:58 +0200

    Readingmaterial epsiodenam wioth viewbag

--- SecurityEducation/Controllers/ReadingMaterialController.cs
+++ SecurityEducation/Controllers/ReadingMaterialController.cs
@@ -6,17 +6,21 @@ namespace SecurityEducation.Controllers
     public class ReadingMaterialController : Controller
     {
         private readonly IReadingMaterialService _readingMaterialService;
+        private readonly IEpisodeService _episodeService;
 
-        public ReadingMaterialController(IReadingMaterialService readingMaterialService)
+        public ReadingMaterialController(IReadingMaterialService readingMaterialService, IEpisodeService episodeService)
         {
             _readingMaterialService = readingMaterialService;
+            _episodeService = episodeService;
         }
 
         [HttpGet("/ReadingMaterial/ReadingMaterials/{chapterId}/{episodeId}")]
         public async Task<IActionResult> ReadingMaterials(int chapterId, int episodeId)
         {
             var model = await _readingMaterialService.GetReadingMaterialByEpisodeId(episodeId);
+            var episode = await _episodeService.GetEpisodeById(episodeId, chapterId);
 
+            ViewBag.EpisodeName = episode.Name;
             ViewBag.ChapterId = chapterId;
             return View(model);
         }
